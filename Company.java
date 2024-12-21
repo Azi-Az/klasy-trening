1254e3f9fbab48b6ec6d2348ed6741bfc74d4658
Cezixx <cmarkiewicz60@gmail.com>
2024-12-21 09:47:41 +0100

    dodanie listy do firmy

--- Company.java
+++ Company.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Company {
     private String name;
+    private List<Employee> employees;
 
 
     public Company(String name) {
         this.name = name;
+        this.employees = new ArrayList<>();
     }
 
     public String getName() {
@@ -12,6 +17,10 @@ public class Company {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
     }
 
     }
--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main (String[] args) {
+
         Person firstPerson = new Person("Natalia", "Test");
         Employee firstEmployee = new Employee(firstPerson, Departament.SALES);
 
@@ -12,13 +13,32 @@ public class Main {
         Person thirdPerson = new Person("Urszula", "Demo");
         Employee thirdEmployee = new Employee(thirdPerson, Departament.ADMINISTRATION);
 
-        List<Employee> employees = new ArrayList<>();
-
+        Company company = new Company("Firma");
+        List<Employee> employees = company.getEmployees();
         employees.add(firstEmployee);
         employees.add(secondEmployee);
         employees.add(thirdEmployee);
 
-        System.out.println(employees);
+        for (int i = 0; i < company.getEmployees().size(); i++){
+            System.out.println(employees.get(i).toString());
+        }
+
+
+
+
+
+
+//        List<Employee> employees = new ArrayList<>();
+//
+//        employees.add(firstEmployee);
+//        employees.add(secondEmployee);
+//        employees.add(thirdEmployee);
+//
+//        for (int i = 0; i < employees.size(); i++) {
+//            System.out.println(employees.get(i).toString());
+//        }
+
+
    }
 }
 
